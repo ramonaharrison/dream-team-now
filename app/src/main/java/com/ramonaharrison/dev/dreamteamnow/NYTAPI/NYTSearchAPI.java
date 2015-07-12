aa2caeab52961dfbc63fea306f8d343e2ffcb769
KMaragh <ksmaragh@gmail.com>
2015-07-12 10:43:22 -0400

    Added Todo for news search API

--- app/src/main/java/com/ramonaharrison/dev/dreamteamnow/NYTAPI/NYTSearchAPI.java
+++ app/src/main/java/com/ramonaharrison/dev/dreamteamnow/NYTAPI/NYTSearchAPI.java
@@ -8,6 +8,8 @@ import retrofit.http.GET;
  */
 public interface NYTSearchAPI {
 
+//TODO: Need to fix retrofit query fields, right now the search is hardcoded for demo purposes.
+//    Example 'GET'
 //    @GET("/articlesearch.json?q={searchterm}&facet_field=day_of_week&begin_date={begindate}&end_date={enddate}&api-key=9e8ea4b3395b35879808c8bc47017370:12:72391617")
 
 @GET("/articlesearch.json?q=romney&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key=9e8ea4b3395b35879808c8bc47017370:12:72391617")
